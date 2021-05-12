5526ff0dfa1398242a42d2381e91976e006f64e2
Adrian Gois <adrian.gois@gmail.com>
2021-05-11 23:29:08 -0300

    Inclusao de exemplo de SQS

--- src/main/groovy/br/com/pismo/casepismo/LoadOperationsType.groovy
+++ src/main/groovy/br/com/pismo/casepismo/LoadOperationsType.groovy
@@ -2,7 +2,14 @@ package br.com.pismo.casepismo
 
 import br.com.pismo.casepismo.models.OperationsType
 import br.com.pismo.casepismo.repositories.OperationsTypeRepository
+import br.com.pismo.casepismo.services.AccountService
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
@@ -13,11 +20,32 @@ class LoadOperationsType implements ApplicationRunner {
     @Autowired
     OperationsTypeRepository operationsTypeRepository
 
+    @Autowired
+    AccountService accountService
+    @Value("#{environment.aws_accessKey}")
+    String awsKey
+
+    @Value("#{environment.aws_pass}")
+    String awsPass
+
+    @Value("\${sqs.url}")
+    String sqsUrl
+
     @Override
     void run(ApplicationArguments args) throws Exception {
         operationsTypeRepository.save(new OperationsType(id: 1, description: "COMPRA A VISTA"))
         operationsTypeRepository.save(new OperationsType(id: 2, description: "COMPRA PARCELADA"))
         operationsTypeRepository.save(new OperationsType(id: 3, description: "SAQUE"))
         operationsTypeRepository.save(new OperationsType(id: 4, description: "PAGAMENTO"))
+
+        AWSCredentialsProvider provider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsKey, awsPass))
+        AmazonSQS amazonSQS = AmazonSQSClientBuilder.standard().withRegion("us-east-2").withCredentials(provider).build()
+        while (true) {
+            amazonSQS.receiveMessage(sqsUrl).getMessages().each {
+                println """************** ${it.Body}"""
+                final String messageHandle = it.getReceiptHandle()
+                amazonSQS.deleteMessage(sqsUrl, messageHandle)
+            }
+        }
     }
 }
--- src/main/groovy/br/com/pismo/casepismo/controllers/AccountController.groovy
+++ src/main/groovy/br/com/pismo/casepismo/controllers/AccountController.groovy
@@ -4,7 +4,15 @@ import br.com.pismo.casepismo.exceptions.AccountExceptions
 import br.com.pismo.casepismo.models.Account
 
 import br.com.pismo.casepismo.services.AccountService
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.sqs.AbstractAmazonSQS
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.amazonaws.services.sqs.model.SendMessageResult
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -18,6 +26,15 @@ class AccountController {
 
     @Autowired
     AccountService accountService
+    @Value("#{environment.aws_accessKey}")
+    String awsKey
+
+    @Value("#{environment.aws_pass}")
+    String awsPass
+
+    @Value("\${sqs.url}")
+    String sqsUrl
+
 
     @PostMapping('')
     def create(@RequestBody Account account) throws Exception {
@@ -26,6 +43,7 @@ class AccountController {
 
     @GetMapping('{id}')
     def accountById(@PathVariable long id) {
+        this.notificarFila("""Acesso a conta com id= ${id}""")
         accountService.findById(id)
     }
 
@@ -36,5 +54,12 @@ class AccountController {
         else {
             new ResponseEntity<Object>(new AccountExceptions(ex.message, HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR)
         }
+    }
+
+    private notificarFila(String message){
+        AWSCredentialsProvider provider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsKey, awsPass))
+        AmazonSQS amazonSQS = AmazonSQSClientBuilder.standard().withRegion("us-east-2").withCredentials(provider).build()
+        SendMessageResult  result = amazonSQS.sendMessage(sqsUrl, message)
+
     }
 }
